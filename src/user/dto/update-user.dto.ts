a5339fd20aae87b92d0ba6f7f9bd9defa56273f5
mazharul90007 <mazharul90007@gmail.com>
2026-02-06 16:20:45 +0600

    Swagger code added in User apis and dtos

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsEmail,
   IsEnum,
@@ -9,28 +10,46 @@ import {
 import { UserRole, UserStatus } from 'generated/prisma/enums';
 
 export class UpdateUserDto {
+  @ApiPropertyOptional({
+    example: 'sourabh@gmail.com',
+    description: 'User email address',
+  })
   @IsOptional()
   @IsString()
   @IsEmail()
   email?: string;
 
+  @ApiPropertyOptional({
+    example: '123456',
+    description: 'new at least 6 digit password',
+  })
   @IsOptional()
   @IsString()
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
   password?: string;
 
+  @ApiPropertyOptional({
+    example: 'Mazharul Islam',
+    description: 'User updated name',
+  })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiPropertyOptional({
+    example: '01746327...',
+    description: 'User updated phone number',
+  })
   @IsOptional()
   @IsString()
   phone?: string;
 
+  @ApiPropertyOptional({ enum: UserRole, example: UserRole.LEADER })
   @IsOptional()
   @IsEnum(UserRole)
   role?: UserRole;
 
+  @ApiPropertyOptional({ enum: UserStatus, example: UserStatus.ACTIVE })
   @IsOptional()
   @IsEnum(UserStatus)
   status?: UserStatus;
--- src/user/dto/user-filter.dto.ts
+++ src/user/dto/user-filter.dto.ts
@@ -1,24 +1,44 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsNumberString, IsOptional, IsString } from 'class-validator';
 // import { UserRole, UserStatus } from 'src/generated/prisma/enums';
 import { UserRole, UserStatus } from 'generated/prisma/enums';
 
 export class UserFilterDto {
+  @ApiPropertyOptional({ enum: UserRole, description: 'Filter by user role' })
   @IsOptional()
   @IsEnum(UserRole)
   role?: UserRole;
 
+  @ApiPropertyOptional({
+    enum: UserStatus,
+    description: 'Filter by account status',
+  })
   @IsOptional()
   @IsEnum(UserStatus)
   status?: UserStatus;
 
+  @ApiPropertyOptional({
+    example: 'Mazharul',
+    description: 'Search name, email, or phone',
+  })
   @IsOptional()
   @IsString()
   searchTerm?: string;
 
+  @ApiPropertyOptional({
+    example: '1',
+    default: '1',
+    description: 'Page number',
+  })
   @IsOptional()
   @IsNumberString()
   page?: string = '1';
 
+  @ApiPropertyOptional({
+    example: '10',
+    default: '10',
+    description: 'Results per page',
+  })
   @IsOptional()
   @IsNumberString()
   limit?: string = '10';
